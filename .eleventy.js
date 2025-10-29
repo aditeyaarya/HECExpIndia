db1d66d010d8d98e21be2f0cf616f18ae8726073
aditeyaarya <aditeya.arya@zimbra.polytechnique.fr>
2025-10-29 02:08:28 +0100

    Updated index.html

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,9 @@
 module.exports = function (eleventyConfig) {
-  // Copy static assets and data folder to the output
   eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("data");
 
   return {
     dir: {
-      // IMPORTANT: look for templates and _data at the project root
       input: ".",
       includes: "_includes",
       data: "_data",
