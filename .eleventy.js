2409c68993d796e3c50994b1a08ec703bd13feb0
aditeyaarya <aditeya.arya@zimbra.polytechnique.fr>
2025-10-29 02:57:51 +0100

    Updated about

--- .eleventy.js
+++ .eleventy.js
@@ -1,13 +1,14 @@
 module.exports = function (eleventyConfig) {
+  // Copy static assets directly to the output folder
   eleventyConfig.addPassthroughCopy("static");
   eleventyConfig.addPassthroughCopy("data");
 
   return {
     dir: {
-      input: ".",
-      includes: "_includes",
-      data: "_data",
-      output: "_site",
+      input: ".",          // main project folder
+      includes: "_includes", // where layouts/partials go
+      data: "_data",         // where global data files go
+      output: "_site",       // build output folder
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk",
